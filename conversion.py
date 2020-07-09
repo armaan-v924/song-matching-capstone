8e4b2972d8c98f18082fa3b1490655c746d49e01
avakharia924 <52749546+avakharia924@users.noreply.github.com>
2020-07-09 16:37:55 -0400

    conversion rev 1

--- conversion.py
+++ conversion.py
@@ -6,19 +6,19 @@ import numpy as np
 from pathlib import Path
 
 
-def file_to_samples(song,
-                    sampling_rate = 44100):
+def file_to_samples(song, sampling_rate=44100):
     """Converts given song file to numpy array of samples
     
-    Parameters
-    ----------
-    song: str; .mp3 file path  
-
+    Parameters:
+    -----------
+    song: str; .mp3 file path  / accepts pathlib.Path or raw string path
     sampling_rate: int; Sampling Rate of the song, Hz
                         Defaults to 44100 Hz
 
     Returns: 
-    spectogram: 2D numpy array; rows - freqs, columns - times, elements - 
+    --------
+    spectrogram: 2D numpy array; rows - freqs, columns - times, elements - Fourier coefficients
+    rate: int; sampling rate
 
     """
     samples, rate = lib.load(song, sr=sampling_rate, mono=True)
@@ -32,18 +32,25 @@ def file_to_samples(song,
 
     return spectrogram, rate
 
-def mic_to_samples(duration,
-                   sampling_rate = 44100):
+def mic_to_samples(duration: float):
     """Records audio sample
 
     Parameters
     ----------
     duration: clip length; 
-    sampling_rate: int; Sampling Rate of the song, Hz
-                        Defaults to 44100 Hz
 
-    Returns: samples, sampling_rate
+    Returns: 
+    --------
+    spectrogram: 2D numpy array; rows - freqs, columns - times, elements - Fourier coefficients
+    rate: int; sampling rate
     """
-    pass
-
-
+    frames, rate = record_audio(duration)
+    samples = np.hstack([np.frombugger(i, np.int16) for i in frames])
+    spectrogram, freqs, times = mlab.specgram(
+        samples,
+        NFFT=4096,
+        Fs=rate,
+        window=mlab.window_hanning,
+        noverlap=int(4096 / 2)
+    )    
+    return spectrogram, rate
