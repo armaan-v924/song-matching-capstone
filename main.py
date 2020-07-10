4afacd736df3f4bc2b301b6b8f5e360dc2f95825
avakharia924 <52749546+avakharia924@users.noreply.github.com>
2020-07-10 14:43:16 -0400

    Added database.P, update Main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print("What would you like to do?")
 function = input("1. Add a Song\n2. Find a song\n")
 if function == '1':
     song_path = input("Please enter the full path to the .mp3 file: ")
-    intFunc.add_song(song_path.encode('unicode_escape'))
+    intFunc.add_song(song_path.encode('unicode_escape'), 'database.P')
 elif function == '2':
     duration = input("How long is your song clip?: ")
     intFunc.find_song(duration)
