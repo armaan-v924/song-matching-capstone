81042f7aeaa6151436658954aebd2f4dfcdc2fea
dhan0779 <david2003han@gmail.com>
2020-07-10 10:30:37 -0400

    made changes

--- manageFingerprints.py
+++ manageFingerprints.py
@@ -15,9 +15,9 @@ def add_fingerprints(fingerprints, song_id, database):
 
     Returns
     --------
-    Updated fingerprint database including all of the fingerprints and song_id
+    Updated fingerprint database including all of the new fingerprints and song_id
     '''
-    
+
     for fingerprint in fingerprints:
         if fingerprint in database:
             database[fingerprint] = database[fingerprint].append(song_id)
@@ -28,14 +28,23 @@ def add_fingerprints(fingerprints, song_id, database):
 
 def tally_fingerprints(pairings, database):
     '''
-    params: pairings, database
-    returns: tallies - dictionary key: song_id, values: tallies of each song_id
-    '''
+    Tallies all of the songs that contain each pairing in pairings (fingerprints)
 
+    Returns a dictionary that contains a list of songs and the number of tallies (key: song_id, value: tallies)
+
+    Parameters
+    -----------
+    pairings: a numpy array containing fingerprints
+    database: database of fingerprints (key: fingerprint, value: list of song_ids
+
+    Returns
+    --------
+    tallies - a dictionary that is keeping track of the tallies (key: song_ids, value: number of tallies)
+    '''
 
     tallies = {}
     for pairing in pairings:
-        song_ids = database[pairings]
+        song_ids = database[pairing]
         for song_id in song_ids:
             if song_id in tallies:
                 tallies[song_id] = tallies[song_id] + 1
@@ -43,4 +52,41 @@ def tally_fingerprints(pairings, database):
                 tallies[song_id] = 1
     return tallies
 
-def
+def add_song_fingerprints(database, fingerprints, song_id):
+    '''
+    Keeps track of all the fingerprints in a song
+    
+    Returns a database with keys: song_id and values: numpy array of fingerprints
+    
+    Parameters
+    -----------
+    database: database with keys: song_id and values: numpy array of fingerprints
+    fingerprints: numpy array of fingerprints for a song
+    song_id: id of a certain song
+    
+    Returns
+    --------
+    database containing the updated fingerprints and song_id
+    '''
+    database[song_id] = fingerprints
+    return database
+
+def find_song_id(tallies, threshold,database):
+    '''
+    finds the song_id with the max value of tallies and sees if that value is above the threshold
+
+    Returns song_id if there is a match, otherwise returns "No match found"
+
+    Parameters
+    -----------
+    tallies: dictionary with keys: song_id and values: number of tallies
+    threshold: a percentage to see if there are enough tallies to consider a match
+    database: database with key: song_id and value: numpy array of fingerprints
+
+    Returns
+    --------
+    returns song_id if there is a match or "No match found"
+    '''
+
+    song_id = max(tallies, key=tallies.get)
+    return song_id if tallies[song_id] / len(database[song_id]) >= threshold else "No match found"
