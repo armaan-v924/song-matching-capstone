b8136784ec2d4ad66182318c232e8186ab08e28a
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-10 12:09:46 -0400

    Update interface_functions.py

--- interface_functions.py
+++ interface_functions.py
@@ -24,6 +24,11 @@ from microphone import record_audio
 import librosa as lib
 import matplotlib.mlab as mlab
 from pathlib import Path
+import conversion as c
+import find_peaks as fp
+import manageFingerprints as mf
+import song_metadata as sm
+
 metadata = {}
 database = {}
 
@@ -37,19 +42,24 @@ def meta_load(file_name):
         metadata, database  = pickle.load(opened_file)
 def add_song(mp3_file_path):
     # put mp3 into tuple, unpack it, then generates song_id .
-    spectrogram, rate  = file_to_samples(mp3_file_path)
-    fingerprints = 9
+    spectrogram, rate  = c.file_to_samples(mp3_file_path)
+    fingerprints = fp.create_fingerprints(spectrogram, rate, len(spectrogram))
     song_id = uuid.uuid1()
     #adds fingerprints and ids into 
-    database = add_fingerprints(fingerprints, song_id, database)
-    tallies = tally_fingerprints(fingerprints, database)
-    song_id = find_song_id(tallies, 77)
+    database = mf.add_fingerprints(fingerprints, song_id, database)
     #add metadata = returns dictionary of values used. Then put it as a value of random song_id
-    meda = add_metadata()
+    meda = sm.add_metadata()
     metadata.add(song_id, meda)
+def find_song(recording):
+    #mic_to_samples -> fingerprint -> tally -> highest tally (find_song_id)
+    spectrogram, rate = c.mic_to_samples(recording)
+    fingerprints = fp.create_fingerprints(peak_locations, rate, len(spectrogram)):
+    tallies = mf.tally_fingerprints(fingerprints, database)
+    song_id = mf.find_song_id(tallies, 0.05 ,metadata)
+    print("You are currently listening to \"" + metadata[song_id][0] + "\" by " + metadata[song_id][1] + ". Genre: " + metadata[song_id][2]
 def print_song_database():
     print("List of Songs")
     print("------------------")
     for key in metadata:
-        print("\"" + metadata[key][0] +"\" by \"" + metadata[key][1] + "\". Genre: " + metadata[key][2])
+        print("\"" + metadata[key][0] +"\" by  + metadata[key][1] + . Genre: " + metadata[key][2])
     print("------------------")
